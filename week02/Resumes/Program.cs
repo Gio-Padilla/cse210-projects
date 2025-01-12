aec17bdc0c0df0568a101edf3e4ff5ff5349abd5
Gio-Padilla <pad24005@byui.edu>
2025-01-12 13:05:29 -0600

    Resume Project

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2010;
+        job1._endYear = 2019;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Front-End Web Developer";
+        job2._startYear = 2019;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Giovaughni Padilla";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
