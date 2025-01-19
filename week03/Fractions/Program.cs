2da523824c6d6af221a3c75ba7f45903fdcacee2
Gio-Padilla <pad24005@byui.edu>
2025-01-19 16:11:01 -0600

    Fractions for W3 is finished

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Clear();
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction theFraction1 = new Fraction();
+        Console.WriteLine(theFraction1.GetFractionString());
+        Console.WriteLine(theFraction1.GetDecimalValue());
+
+        Fraction theFraction2 = new Fraction(5);
+        Console.WriteLine(theFraction2.GetFractionString());
+        Console.WriteLine(theFraction2.GetDecimalValue());
+
+        Fraction theFraction3 = new Fraction(3, 4);
+        Console.WriteLine(theFraction3.GetFractionString());
+        Console.WriteLine(theFraction3.GetDecimalValue());
+
+        Fraction theFraction4 = new Fraction(1, 3);
+        Console.WriteLine(theFraction4.GetFractionString());
+        Console.WriteLine(theFraction4.GetDecimalValue());
     }
 }
